c10bd55bd41afca41477d95084aa01f10ce4a8c4
salman.taj <salman.taj@innroad.com>
2022-03-10 20:02:07 +0500

    End of Step # 6

--- src/BBBankUI/src/app/services/blog.service.ts
+++ src/BBBankUI/src/app/services/blog.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Blog } from '../models/blog';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
-  constructor() { }
+  // depdendency injecting HttpClient
+  constructor(private httpClient: HttpClient) { }
+
+  // returning type safe Blog array
+  getBlogPosts(): Observable<Blog[]> {
+    // calling HttpClient get method to call the API
+    // contructing the URL using the environment variable and path to the function (i.e., posts)
+    return this.httpClient.get<Blog[]>(environment.baseUrl + 'posts');
+  }
 }
