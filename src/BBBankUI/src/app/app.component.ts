6d2cc149dded5c49dbaa89d47a753d17118951a6
salman.taj <salman.taj@innroad.com>
2022-03-10 20:05:08 +0500

    End of Step # 8

--- src/BBBankUI/src/app/app.component.ts
+++ src/BBBankUI/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Blog } from './models/blog';
+import { BlogService } from './services/blog.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'BBBankUI';
+export class AppComponent implements OnInit {
+
+  // declare and initialize an empty type safe array
+  blogs: Blog[] = [];
+
+  // dependency injecting the Blog Service
+  constructor(private blogService: BlogService) {
+  }
+
+  ngOnInit(): void {
+
+    // calling the service
+    this.blogService.getBlogPosts()
+      // filtering the top 10 posts based on criteria using lambda expression
+      .pipe(map(p => p.filter(x => x.id < 10)))
+      .subscribe(response => {
+        this.blogs = response;
+      });
+  }
+
 }
